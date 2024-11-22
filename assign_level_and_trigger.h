6b2a001885e45c8f0f73bfd525987309b52b5d94
AliRazaBaloch05 <arbaloch2005@gmail.com>
2024-11-22 20:38:04 +0500

    Added relevant adder for assign_and_trigger.c

---
+++ assign_level_and_trigger.h
@@ -0,0 +1,5 @@
+#ifndef ASSIGN_LEVEL_AND_TRIGGER_H
+#define ASSIGN_LEVEL_AND_TRIGGER_H
+int assign_level(ProcessedData data, const char *output_file);
+void trigger_alert(int danger_level);
+#endif //ASSIGN_LEVEL_AND_TRIGGER_H
